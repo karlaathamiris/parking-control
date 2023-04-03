3c0e367ae5c43687a5c5255d807b5c97df4fcebf
karlaathamiris <karlatn@outlook.com>
2023-04-02 23:08:25 -0300

    paginação e finalização da API

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -2,12 +2,16 @@ package com.api.parkingcontrol.services;
 
 import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
-
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-import org.springframework.stereotype.Service;
+
 
 
 /**
@@ -44,7 +48,17 @@ public class ParkingSpotService {
 		return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
 	}
 
-	public List <ParkingSpotModel> findAll() {
-		return parkingSpotRepository.findAll();
+	public Page<ParkingSpotModel> findAll (Pageable pageable){
+		return parkingSpotRepository.findAll(pageable);
+	}
+
+	public Optional<ParkingSpotModel> findById(UUID id) {
+		return parkingSpotRepository.findById(id);
+	}
+
+	@Transactional
+	public void delete(ParkingSpotModel parkingSpotModel) {
+		parkingSpotRepository.delete(parkingSpotModel);
+		
 	}
 }
